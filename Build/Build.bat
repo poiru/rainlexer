57e88bc8688bbaa9d58d25acc62f048e9986df1f
Birunthan Mohanathas <birunthan@mohanathas.com>
2014-09-20 21:44:23 +0300

    Bump version to 1.1.15

--- Build/Build.bat
+++ Build/Build.bat
@@ -5,7 +5,7 @@ set MAKENSIS=%PROGRAMFILES%\NSIS\MakeNSIS.exe
 
 set VERSION_MAJOR=1
 set VERSION_MINOR=1
-set VERSION_SUBMINOR=14
+set VERSION_SUBMINOR=15
 
 if not exist "%VCVARSALL%" echo ERROR: vcvarsall.bat not found & goto END
 call "%VCVARSALL%" x86 > nul
