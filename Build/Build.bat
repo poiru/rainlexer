613814ddbfcaffa6073af5ea19ff8519c65fae09
jsmorley <jsmorley@gmail.com>
2015-05-24 09:21:28 -0400

    Added keywords for Loop measure and its options.

--- Build/Build.bat
+++ Build/Build.bat
@@ -5,7 +5,7 @@ set MAKENSIS=%PROGRAMFILES%\NSIS\MakeNSIS.exe
 
 set VERSION_MAJOR=1
 set VERSION_MINOR=1
-set VERSION_SUBMINOR=18
+set VERSION_SUBMINOR=19
 
 if not exist "%VCVARSALL%" echo ERROR: vcvarsall.bat not found & goto END
 call "%VCVARSALL%" x86 > nul
