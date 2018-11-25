3c7b283000806a8def5f6277bf78dfb74c7cee79
jsmorley <jsmorley@gmail.com>
2018-11-25 10:37:26 -0500

    Updated Version.h

--- RainLexer/Version.h
+++ RainLexer/Version.h
@@ -1,4 +1,4 @@
 #pragma once
-#define RAINLEXER_VERSION_RC 2,0,6,0
-#define RAINLEXER_VERSION_STRING "2.0.6.0"
-#define RAINLEXER_TITLE L"RainLexer 2.0.6"
+#define RAINLEXER_VERSION_RC 2,1,0,0
+#define RAINLEXER_VERSION_STRING "2.1.0.0"
+#define RAINLEXER_TITLE L"RainLexer 2.1.0"
