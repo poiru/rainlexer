870fcef51a9b7514ee49cfbd4edba5c2b0db6c0e
jsmorley <jsmorley@gmail.com>
2020-07-04 09:16:10 -0400

    Added new SysInfo features. LAST_SLEEP_TIME LAST_WAKE_TIME

--- Build/Build.bat
+++ Build/Build.bat
@@ -7,7 +7,7 @@ set MAKENSIS=%PROGRAMFILES%\NSIS\MakeNSIS.exe
 set GIT=%PROGRAMFILES%\Git\bin\git.exe
 
 set VERSION_MAJOR=2
-set VERSION_MINOR=13
+set VERSION_MINOR=14
 set VERSION_SUBMINOR=0
 
 if not exist "%VCVARSALL%" echo ERROR: vcvarsall.bat not found & goto END
