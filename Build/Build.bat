a185ae26e7a9cc9551fe278e8bc72bc5c585fff6
jsmorley <jsmorley@gmail.com>
2022-02-13 11:06:22 -0500

    Updates for 2.19.0

--- Build/Build.bat
+++ Build/Build.bat
@@ -7,7 +7,7 @@ set MAKENSIS=%PROGRAMFILES%\NSIS\MakeNSIS.exe
 set GIT=%PROGRAMFILES%\Git\bin\git.exe
 
 set VERSION_MAJOR=2
-set VERSION_MINOR=18
+set VERSION_MINOR=19
 set VERSION_SUBMINOR=0
 
 if not exist "%VCVARSALL%" echo ERROR: vcvarsall.bat not found & goto END
